5e63c73b69c8e050ec70ae4f23a24e6bf8214ea2
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-05-21 03:41:57 -0700

    updates

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/default/config.h
@@ -22,6 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 
 #define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_RIGHT
 //#define EE_HANDS
 // Rows are doubled-up
+
+#define SELECT_SOFT_SERIAL_SPEED 2
--- keyboards/handwired/dactyl_manuform/6x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/default/keymap.c
@@ -17,11 +17,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      LALT_T(KC_TAB) , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,               KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,RALT_T(KC_BSLS),
      LCTL_T(KC_ESC) , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,               KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,RCTL_T(KC_QUOT),
      KC_HENK        , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,               KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT ,
-                             KC_MINS,KC_PLUS,                                              KC_LBRC,KC_RBRC,
+                             KC_MINS,KC_EQL ,                                              KC_LBRC,KC_RBRC,
 
-                                      LSFT_T(KC_TAB),KC_SPC,                KC_BSPC,       KC_RSFT,
-                                      KC_ESC ,LGUI_T(KC_F13),               RCTL_T(KC_ENT),KC_DEL ,
-                                      KC_CAPS, LOWER ,                      KC_RALT,       KC_INS
+                                     LSFT_T(KC_TAB) ,KC_SPC ,               KC_BSPC,       LSFT_T(KC_ENT),
+                                     KC_ESC ,LGUI_T(KC_F13),                RCTL_T(KC_GRV),KC_DEL ,
+                                     KC_CAPS, LOWER ,                       KC_RALT,       KC_INS
   ),
 
   [_LOWER] = LAYOUT_6x6(
