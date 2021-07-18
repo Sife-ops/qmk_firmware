dfd4fbc3c1411d982ccde7cc679fa38505955790
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-07-18 02:58:24 -0700

    update

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/default/keymap.c
@@ -12,32 +12,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* [_QWERTY] = LAYOUT_6x6( */
-
-  /*   KC_F1          , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,            KC_F7 , KC_F8 , KC_F9 ,KC_F10  ,KC_F11 ,KC_F12 , */
-  /*   KC_ESC         , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,            KC_6  , KC_7  , KC_8  , KC_9   , KC_0  ,KC_MINS, */
-  /*   LALT_T(KC_GRV) , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,            KC_Y  , KC_U  , KC_I  , KC_O   , KC_P  ,RALT_T(KC_BSLS), */
-  /*   LCTL_T(KC_ESC) , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,            KC_H  , KC_J  , KC_K  , KC_L   ,KC_SCLN,RCTL_T(KC_QUOT), */
-  /*   KC_HENK        , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,            KC_N  , KC_M  , KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , */
-  /*                            KC_MINS,KC_EQL ,                                           KC_LBRC,KC_RBRC, */
-
-  /*                                   LSFT_T(KC_TAB),KC_SPC,              RCTL_T(KC_BSPC),LSFT_T(KC_ENT), */
-  /*                                   KC_CLCK,       LGUI_T(KC_ESC),      RALT_T(KC_QUOT),KC_PGUP, */
-  /*                                   NUMPAD,        LOWER,               LCAG_T(KC_TAB), KC_PGDN */
-  /* ), */
-
   [_QWERTY] = LAYOUT_6x6(
 
     KC_F1          , KC_F2 , KC_F3   , KC_F4  , KC_F5 , KC_F6 ,         KC_F7 , KC_F8 , KC_F9   , KC_F10  , KC_F11  , KC_F12 ,
-    KC_LGUI        , KC_1  , KC_2    , KC_3   , KC_4  , KC_5  ,         KC_6  , KC_7  , KC_8    , KC_9    , KC_0    , RGUI_T(KC_BSPC) ,
-    LALT_T(KC_GRV) , KC_Q  , KC_W    , KC_E   , KC_R  , KC_T  ,         KC_Y  , KC_U  , KC_I    , KC_O    , KC_P    , RALT_T(KC_BSLS) ,
+    KC_LGUI        , KC_1  , KC_2    , KC_3   , KC_4  , KC_5  ,         KC_6  , KC_7  , KC_8    , KC_9    , KC_0    , KC_RGUI ,
+    LALT_T(KC_TAB) , KC_Q  , KC_W    , KC_E   , KC_R  , KC_T  ,         KC_Y  , KC_U  , KC_I    , KC_O    , KC_P    , RALT_T(KC_ENT) ,
     LCTL_T(KC_ESC) , KC_A  , KC_S    , KC_D   , KC_F  , KC_G  ,         KC_H  , KC_J  , KC_K    , KC_L    , KC_SCLN , RCTL_T(KC_QUOT) ,
-    KC_HENK        , KC_Z  , KC_X    , KC_C   , KC_V  , KC_B  ,         KC_N  , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT ,
-                             KC_MINS , KC_EQL ,                                        KC_LBRC , KC_RBRC ,
+    KC_GRV         , KC_Z  , KC_X    , KC_C   , KC_V  , KC_B  ,         KC_N  , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_BSLS ,
+                             KC_MINS , KC_EQL ,                                         KC_LBRC , KC_RBRC ,
 
     KC_LSFT , KC_SPC ,                                                  KC_BSPC       , KC_RSFT,
-    KC_CLCK , KC_TAB ,                                                  KC_ENT        , KC_PGUP,
-    NUMPAD  , LOWER,                                                    LCAG_T(KC_NO) , KC_PGDN
+    KC_HENK , KC_ESC ,                                                  KC_ENT        , KC_PGUP,
+    NUMPAD  , LOWER  ,                                                  LCAG_T(KC_NO) , KC_PGDN
 
   ),
 
